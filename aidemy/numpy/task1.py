054556ecd23d491a4d3daf2b9884072c60e400d4
yasuno0327 <5315076@ed.tus.ac.jp>
2018-11-10 14:07:30 +0900

    aidemy num

---
+++ aidemy/numpy/task1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+storages = [24, 3, 4, 23, 10, 12]
+print(storages)
+
+# ndarray配列を生成し、変数np_storagesに代入してください
+np_storages = np.array(storages) # => listをndarrayに変換
+print(np_storages)
+
+# 変数np_storagesの型を出力してください
+print(type(np_storages))
